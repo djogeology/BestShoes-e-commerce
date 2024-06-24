810ab62178c512ecd1ee288553971a95c79d062f
djogeology <bzwajih@gmail.com>
2024-06-24 14:55:23 +0100

    server on run

---
+++ backEnd/Database/index.js
@@ -0,0 +1,13 @@
+
+  const mysql = require("mysql2");
+const mysqlConfig = require("./config.js");
+
+const connection = mysql.createConnection(mysqlConfig);
+
+connection.connect((err) => {
+  if (err) console.log("Error to connect to database", err);
+  else console.log("My Database is connected!!");
+});
+
+// Your Database Queries Here!!
+module.exports=connection
